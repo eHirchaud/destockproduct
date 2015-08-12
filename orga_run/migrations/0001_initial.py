0f47d379c8d6bd11b994a526db0144229e2e54a0
Edouard Hirchaud <edouard.hirchaud@anses.fr>
2015-08-12 16:23:28 +0200

    change list des lots

--- orga_run/migrations/0001_initial.py
+++ orga_run/migrations/0001_initial.py
@@ -2,19 +2,21 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='ActionProject',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
-                ('date_start', models.DateTimeField(auto_now_add=True, verbose_name="Date d'action")),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('date_start', models.DateTimeField(verbose_name="Date d'action", auto_now_add=True)),
                 ('action_description', models.TextField()),
             ],
             options={
@@ -22,9 +24,27 @@ class Migration(migrations.Migration):
             bases=(models.Model,),
         ),
         migrations.CreateModel(
-            name='Application',
+            name='EntryDestock',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('count_react', models.FloatField(default=0)),
+                ('cost_real', models.FloatField(default=0)),
+                ('cost_reduce', models.FloatField(default=0)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Lot',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('current_stock', models.IntegerField(verbose_name='Nombre de réactions restantes')),
+                ('code_fabricant', models.CharField(max_length=15)),
+                ('code_tmpi', models.CharField(max_length=10)),
+                ('n_lot', models.CharField(max_length=20, null=True)),
+                ('open_date', models.DateTimeField(verbose_name="Date d'ouverture", auto_now=True)),
+                ('peremption_date', models.DateTimeField(verbose_name='Date de péremption', null=True)),
             ],
             options={
             },
@@ -33,7 +53,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Manip',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('name', models.CharField(max_length=100)),
             ],
             options={
@@ -43,15 +63,14 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Product',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('name', models.CharField(max_length=100)),
                 ('ref', models.CharField(max_length=50)),
-                ('real_cost', models.IntegerField()),
-                ('reduce_cost', models.IntegerField()),
-                ('nb_react_by_samples', models.IntegerField()),
-                ('conditionement_nombre', models.DecimalField(decimal_places=2, max_digits=5)),
+                ('real_cost', models.FloatField()),
+                ('reduce_cost', models.FloatField()),
+                ('nb_react_by_samples', models.FloatField()),
+                ('conditionement_nombre', models.FloatField()),
                 ('conditionement_type', models.CharField(max_length=15)),
-                ('reac_by_sample', models.DecimalField(decimal_places=2, max_digits=5)),
                 ('comment', models.TextField()),
             ],
             options={
@@ -59,29 +78,27 @@ class Migration(migrations.Migration):
             bases=(models.Model,),
         ),
         migrations.CreateModel(
-            name='Lots',
+            name='Profil',
             fields=[
-                ('product_ptr', models.OneToOneField(primary_key=True, serialize=False, parent_link=True, auto_created=True, to='orga_run.Product')),
-                ('current_stock', models.IntegerField(verbose_name='Nombre de réactions restantes')),
-                ('code_fabricant', models.CharField(max_length=15)),
-                ('code_tmpi', models.CharField(max_length=10)),
-                ('n_lot', models.CharField(max_length=20, null=True)),
-                ('open_date', models.DateTimeField(verbose_name="Date d'ouverture", auto_now=True)),
-                ('peremption_date', models.DateTimeField(verbose_name='Date de péremption', null=True)),
-                ('manip', models.ManyToManyField(to='orga_run.Manip')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('site_web', models.URLField(blank=True)),
+                ('avatar', models.ImageField(upload_to='avatar/', blank=True, null=True)),
+                ('signature', models.TextField(blank=True)),
+                ('inscrit_newlette', models.BooleanField(default=False)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
             options={
             },
-            bases=('orga_run.product',),
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Project',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('acro', models.CharField(max_length=30)),
                 ('code', models.CharField(max_length=8)),
                 ('client', models.CharField(max_length=50)),
-                ('date_start', models.DateTimeField(auto_now_add=True, verbose_name='Date de création')),
+                ('date_start', models.DateTimeField(verbose_name='Date de création', auto_now_add=True)),
             ],
             options={
             },
@@ -90,7 +107,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Run',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('code', models.CharField(max_length=20)),
                 ('projects', models.ManyToManyField(to='orga_run.Project')),
             ],
@@ -101,10 +118,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Sample',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('code', models.CharField(max_length=20)),
                 ('manips', models.ManyToManyField(to='orga_run.Manip')),
-                ('project', models.ForeignKey(to='orga_run.Project')),
+                ('project', models.ForeignKey(to='orga_run.Project', related_name='samples')),
             ],
             options={
             },
@@ -113,11 +130,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='StepProto',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
-                ('reac_by_samples', models.DecimalField(decimal_places=2, max_digits=5)),
-                ('reac_by_manip', models.DecimalField(decimal_places=2, max_digits=5)),
-                ('manip', models.ForeignKey(to='orga_run.Manip')),
-                ('products', models.ForeignKey(to='orga_run.Product')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('reac_by_sample', models.FloatField(default=0)),
+                ('reac_by_manip', models.FloatField(default=0)),
+                ('manip', models.ForeignKey(to='orga_run.Manip', related_name='steps_proto')),
+                ('product', models.ForeignKey(to='orga_run.Product', related_name='steps_proto')),
             ],
             options={
             },
@@ -130,21 +147,45 @@ class Migration(migrations.Migration):
             preserve_default=True,
         ),
         migrations.AddField(
-            model_name='lots',
+            model_name='lot',
+            name='manip',
+            field=models.ManyToManyField(to='orga_run.Manip', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='lot',
+            name='product',
+            field=models.ForeignKey(null=True, to='orga_run.Product', related_name='lots'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='lot',
             name='samples',
-            field=models.ManyToManyField(to='orga_run.Sample'),
+            field=models.ManyToManyField(to='orga_run.Sample', null=True),
             preserve_default=True,
         ),
         migrations.AddField(
-            model_name='application',
-            name='lot',
-            field=models.ForeignKey(to='orga_run.Product'),
+            model_name='entrydestock',
+            name='manip',
+            field=models.ForeignKey(null=True, to='orga_run.Manip', related_name='entries_destock'),
             preserve_default=True,
         ),
         migrations.AddField(
-            model_name='application',
-            name='protocole',
-            field=models.ForeignKey(to='orga_run.StepProto'),
+            model_name='entrydestock',
+            name='product',
+            field=models.ForeignKey(null=True, to='orga_run.Product', related_name='entries_destock'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='entrydestock',
+            name='project',
+            field=models.ForeignKey(null=True, to='orga_run.Project', related_name='entries_destock'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='entrydestock',
+            name='run',
+            field=models.ForeignKey(null=True, to='orga_run.Run', related_name='entries_destock'),
             preserve_default=True,
         ),
         migrations.AddField(
